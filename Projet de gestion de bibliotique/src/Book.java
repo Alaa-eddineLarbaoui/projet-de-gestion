9a3d8315ef372d8dd42e5503009286ff38722ebd
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-02-07 15:04:30 +0100

    j'ai commencer le projet par la creation de la classe book

---
+++ Projet de gestion de bibliotique/src/Book.java
@@ -0,0 +1,10 @@
+public class Book {
+    private String titre ;
+    private String auteur;
+    private String ISBN;
+    private String Date_de_publication;
+
+    void test(){
+        System.out.println();
+    }
+}
---
+++ Projet de gestion de bibliotique/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1. Ajouter un livre");
+        System.out.println("2. Supprimer un livre");
+        System.out.println("3. Rechercher un livre");
+        System.out.println("4. Sortie");
+        System.out.println("Enter you choice");
+    }
+}
